c1f85c22ff0f5d281b90e6f35c5973d88fabe1ed
ducnhantapdev <ducnhantapdev@gmail.com>
2025-06-07 22:23:04 +0700

    update

--- student-management/src/redux/reducers/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import studentReducer from './studentReducer';
-
-const rootReducer = combineReducers({
-  students: studentReducer,
-});
-
-export default rootReducer;
